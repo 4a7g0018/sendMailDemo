555959eb2b923e90544d4feb4ab3cce7cc6ffcdb
BensonYan <4a7g0018@stust.edu.tw>
2022-01-27 00:00:13 +0800

    + Create User Repository

---
+++ src/main/java/com/yan/send/mail/sendMailDemo/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.yan.send.mail.sendMailDemo.repository;
+
+import com.yan.send.mail.sendMailDemo.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Users,Long> {
+}
