931280910f3934e78d41386e6793aac050218997
BensonYan <4a7g0018@stust.edu.tw>
2022-01-27 01:06:59 +0800

    + Create User Service

---
+++ src/main/java/com/yan/send/mail/sendMailDemo/service/UsersService.java
@@ -0,0 +1,31 @@
+package com.yan.send.mail.sendMailDemo.service;
+
+import com.yan.send.mail.sendMailDemo.model.Users;
+import com.yan.send.mail.sendMailDemo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@Slf4j
+public class UsersService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            Users user = userRepository.findByUserName(username);
+            return new User(user.getUserName(), user.getPassword(), Collections.emptyList());
+        }catch (UsernameNotFoundException e) {
+            throw new UsernameNotFoundException("Username is wrong.");
+        }
+    }
+}
