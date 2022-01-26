1cd595da02c313680d5d9fe15f2c58d5c50c9d5e
BensonYan <4a7g0018@stust.edu.tw>
2022-01-27 01:59:22 +0800

    + add configure Authentication Manager Builder

--- src/main/java/com/yan/send/mail/sendMailDemo/security/Config.java
+++ src/main/java/com/yan/send/mail/sendMailDemo/security/Config.java
@@ -1,12 +1,25 @@
 package com.yan.send.mail.sendMailDemo.security;
 
+import com.yan.send.mail.sendMailDemo.service.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class Config extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UsersService usersService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(usersService)
+                .passwordEncoder(new CustomPasswordEncoder());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
