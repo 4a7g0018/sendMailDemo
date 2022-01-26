998eb799e62da8d2d0df412a0f2b9a654b64e5e5
BensonYan <4a7g0018@stust.edu.tw>
2022-01-26 23:57:23 +0800

    + Create Users Entity

---
+++ src/main/java/com/yan/send/mail/sendMailDemo/model/Users.java
@@ -0,0 +1,28 @@
+package com.yan.send.mail.sendMailDemo.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+}
