f7d7852bc4d1eb442133d883200592b3ea365a02
BensonYan <4a7g0018@stust.edu.tw>
2022-01-26 23:50:10 +0800

    Initial commit

---
+++ src/main/java/com/yan/send/mail/sendMailDemo/SendMailDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yan.send.mail.sendMailDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMailDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yan/send/mail/sendMailDemo/SendMailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yan.send.mail.sendMailDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
